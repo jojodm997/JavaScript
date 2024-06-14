46274fb24b932cabc24a6ac1b0e8ff5359977d9e
Fauzan <fauzandwimulyono@gmail.com>
2024-06-14 18:21:16 +0700

    Learning javascript numbers

---
+++ math2.js
@@ -0,0 +1,17 @@
+let x = "100",
+  y = "200";
+
+let z = y + x; // this doesn't work why? because of the +, the other will work - * /
+
+let p = 100 / "popo"; // It'wasnt accepted in javascriptaka NaN - Not a Number
+
+let o = 1 / 0; // Infinity
+
+let d = 0xff; // Hexadecimal
+
+let u = 123;
+let t = new Number(123); // Number as an object
+// == are equal
+// === are not equal
+
+document.getElementById("demo").innerHTML = u == t;

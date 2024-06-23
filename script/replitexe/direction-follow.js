fe788c010fe053a4c1503c859306929716da2b3d
Fauzan <fauzandwimulyono@gmail.com>
2024-06-23 22:50:44 +0700

    added

---
+++ script/replitexe/direction-follow.js
@@ -0,0 +1,8 @@
+const birthYear = 2006;
+const thisYear = 2024;
+const age = thisYear - birthYear;
+const firstName = " Fauzan ";
+const lastName = "Mulyono ";
+const fullName = firstName + lastName;
+
+greeting = "Hello My Name is" + fullName + "and I am " + age + " Years old";

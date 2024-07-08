4eceaa0449d0fad24052f4df50ef40cd04993952
Fauzan <fauzandwimulyono@gmail.com>
2024-07-08 18:34:35 +0700

    Added some stuff

--- script/script1.js
+++ script/script1.js
@@ -7,21 +7,3 @@ alert(admin);
 
 // let name, admin;
 // (name = "john"), (admin = name), alert(admin);
-
-// example
-let ourPlanet = "Earth";
-let currentUserName = "John";
-
-// Manipulated string
-const myText = "I am a string";
-const newString = myText.replace("String", "Sausage");
-console.log(newString);
-
-// Manipulated array
-const myArray = ["I", "love", "chocolate", "frogs"];
-const madeAString = myArray.join("");
-console.log(madeAString);
-
-// Generate randomm number
-const myNumber = Math.random();
-document.getElementById("demo").innerHTML = myNumber;

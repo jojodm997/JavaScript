03bbd949e696b3820f70a1e86686015e47494e59
Fauzan <fauzandwimulyono@gmail.com>
2024-07-16 23:23:23 +0700

    m

--- script/function.js
+++ script/function.js
@@ -1,3 +1,12 @@
-function showMessage() {
-  alert("Helo world");
+function showMessage(from, text = "Hoolaaaaa") {
+  from = "*" + from + "*";
+  alert(from + ":" + text);
+}
+let from = "Ann";
+showMessage(from, "Hello");
+
+alert(from);
+
+function checkAge(age) {
+  return age > 18 ? true : confirm("Did your parents allow you?");
 }

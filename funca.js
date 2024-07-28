d60b2745aa2ba4251bfc78a2e5806f141404c187
Fauzan <fauzandwimulyono@gmail.com>
2024-07-28 21:11:06 +0700

    Adding some script

---
+++ funca.js
@@ -0,0 +1,7 @@
+function checkAge(age) {
+  return age > 18 ? true : confirm('Did your parents allow you in?');
+}
+
+function checkAgee(agee) {
+  return agee > 18 ||  confirm("Did your parents allow you in ?");
+}

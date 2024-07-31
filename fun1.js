5625b0c6c90f31c56fe71a064c58235333e8747e
Fauzan <fauzandwimulyono@gmail.com>
2024-07-31 23:38:34 +0700

    Added js

---
+++ fun1.js
@@ -0,0 +1,7 @@
+function add7 (num, sev = 7) {
+  return num + sev;
+
+}
+
+const result = add7(10);
+console.log(result);
---
+++ fun2.js
@@ -0,0 +1,6 @@
+function multiply (num1, num2){
+  return num1 * num2;
+}
+
+const result = multiply(2,2);
+console.log(result)

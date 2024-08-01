231bd9b58dc9f3cb2881403b5f6bbb85c1c506fc
Fauzan <fauzandwimulyono@gmail.com>
2024-08-01 17:43:42 +0700

    Add

--- fun3.js
+++ fun3.js
@@ -1,3 +1,6 @@
-function capitalize (){
-  return;
+function capitalize (text){
+  return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
 }
+
+const result = capitalize("HELLO WORLD");
+console.log(result)
---
+++ fun4.js
@@ -0,0 +1,6 @@
+function lastLetter (text) {
+  return text.charAt(text.length - 1);
+}
+
+const result = lastLetter("Hello world");
+console.log(result);

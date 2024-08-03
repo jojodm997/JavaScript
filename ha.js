874b09faabbfa8cc5efd49cbdf587c64198b2209
Fauzan <fauzandwimulyono@gmail.com>
2024-08-03 16:18:31 +0700

    Added js script

---
+++ ha.js
@@ -0,0 +1,9 @@
+let ask = prompt("How old are you")
+
+function asked () {
+  if ask >= 18 {
+    alert("Come in");
+  } else {
+    alert("Get out");
+  }
+}
---
+++ hak.js
@@ -0,0 +1,8 @@
+const a = 5;
+const b = 10;
+
+function add() {
+  return b;
+}
+
+console.log(add());

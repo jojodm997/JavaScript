11be4e125f5610761aa6ddc5b4effe72d2dcd3b2
Fauzan <fauzandwimulyono@gmail.com>
2024-08-07 21:56:50 +0700

    Added loop

---
+++ script2/loop1.js
@@ -0,0 +1,27 @@
+let i = 0;
+while(i < 3) { // 0 1 2 (counts from zero)
+  console.log(i);
+  i++;
+} 
+
+let i 0;
+do (
+  console.log(i);
+  i++;
+) while (i < 3); // 0 1 2
+
+for (let i = 0; i < 3; i++;) // 0 1 2 let are inline variable
+console.log(i);
+
+let sum = 0;
+while (true) {
+  let value = +prompt("Enter a number", '');
+  if(!value) break; // if the user enters an empty line or cancels the input. It stops the loop immediately, passing control to the first line after the loop. Namely console.log
+  sum += value;
+}
+console.log('Sum: ' + sum);
+
+for(let i= 0; i < 10; i++) {
+  if (i % 2 == 0) continue;
+console.log(i);
+}

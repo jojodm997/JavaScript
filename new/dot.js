3348390e32110a1958939bfe9bf3bca5d2c25aec
Fauzan <fauzandwimulyono@gmail.com>
2024-08-08 19:10:39 +0700

    Added another script

---
+++ new/dot.js
@@ -0,0 +1,17 @@
+function getComputerChoice() {
+   const random = Math.floor(Math.random() * 3)
+  switch(random) {
+    case 0:
+      console.log("Rock");
+      break;
+    case 1:
+      console.log("Scissors");
+      break;
+    case 2:
+      console.log("Paper");
+      break;
+  }
+  return;
+}
+
+console.log(getComputerChoice());

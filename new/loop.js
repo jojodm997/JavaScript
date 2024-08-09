a9f7dec932909b4315758d3ebb88cfc8b177b149
Fauzan <fauzandwimulyono@gmail.com>
2024-08-09 22:17:26 +0700

    Fix typo

--- new/loop.js
+++ new/loop.js
@@ -35,6 +35,7 @@ while (true) {
 console.log('Sum: ' + sum);
 
 for(let i= 0; i < 10; i++) {
+  // if true, skip the remaining part of the body
   if (i % 2 == 0) continue;
-console.log(i);
+  console.log(i); // 1, then 3, 5, 7, 9
 }

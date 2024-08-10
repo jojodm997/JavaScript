7be5fdc8193db032dde214ba52d5543273f4bc8b
Fauzan <fauzandwimulyono@gmail.com>
2024-08-10 11:35:27 +0700

    Add new script

--- new/loop.js
+++ new/loop.js
@@ -34,8 +34,11 @@ while (true) {
 }
 console.log('Sum: ' + sum);
 
-for(let i= 0; i < 10; i++) {
+for (let i = 0; i < 10; i++) {
   // if true, skip the remaining part of the body
   if (i % 2 == 0) continue;
   console.log(i); // 1, then 3, 5, 7, 9
 }
+
+
+

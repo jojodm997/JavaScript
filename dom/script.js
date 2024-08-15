fdbe62252dbf96a1866d214a025e64b2005c35b0
Fauzan <fauzandwimulyono@gmail.com>
2024-08-15 15:02:59 +0700

    Add dom exe

--- dom/script.js
+++ dom/script.js
@@ -1,7 +1,23 @@
 const container = document.querySelector("#container");
 
 const content = document.createElement("div");
-content.classList.add("content");
-content.textContent = "This is the glorious text-content";
+content.classList.add("content"); // this class is css
+content.textContent = "This is the glorious text-content!"; 
+content.setAttribute("style", "color: blue; background: white;") // css
 
 container.appendChild(content);
+
+
+const contentp = document.createElement("p");
+contentp.classList.add("para");
+contentp.textContent = "Hey i'm red!";
+contentp.setAttribute("style", "color: red;");
+
+container.appendChild(contentp);
+
+const contenth = document.createElement("h3");
+contenth.classList.add("med");
+contenth.textContent = "I'm blue h3!";
+contenth.setAttribute("style", "color: blue;");
+
+container.appendChild(contenth);
